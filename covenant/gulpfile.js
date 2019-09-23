6291e9a78d451a3c4b46eabe990a6c4c50d0a9bc
Marshal <marshalityy@gmail.com>
2019-09-23 15:58:34 +0300

    gulp file modified (uglify не работает с ES6)

--- covenant/gulpfile.js
+++ covenant/gulpfile.js
@@ -1,59 +1,22 @@
 const gulp         = require('gulp');
-const browserSync  = require('browser-sync');
 const concat       = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS     = require('gulp-clean-css');
-const uglify       = require('gulp-uglify');
 const del          = require('del');
 
 const cssFiles = [
     'src/css/main.css'  
 ];
 
-const jsFiles = [
-    'src/js/main.js'
-];
-
 const styles = () => {
     return gulp.src(cssFiles)
     .pipe(concat('main.css'))
-    .pipe(autoprefixer({
-        cascade: false
-    }))
     .pipe(cleanCSS({
         level: 2
     }))
-    .pipe(gulp.dest('build/css/'))
-    .pipe(browserSync.stream());
-};
-
-const scripts = () => {
-    return gulp.src(jsFiles)
-    .pipe(concat('main.js'))
-    .pipe(uglify({
-        toplevel: true
-    }))
-    .pipe(gulp.dest('build/js'))
-    .pipe(browserSync.stream());
-};
-
-const watch = () => {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        notify: false
-    });
-
-    gulp.watch('src/css/**/*.css', styles);
-    gulp.watch('src/js/**/*.js', scripts);
-    gulp.watch('./*.html').on('change', browserSync.reload)
+    .pipe(gulp.dest('build/'));
 };
 
 const clean = () => del(['build/*']);
 
 gulp.task('styles', styles);
-gulp.task('scripts', scripts);
-gulp.task('watch', watch);
-gulp.task('build', gulp.series(clean, gulp.parallel(styles, scripts)));
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('build', gulp.series(clean, gulp.parallel(styles)));
