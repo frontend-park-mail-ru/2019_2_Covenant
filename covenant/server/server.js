75a2e9c034a3df64703befb068f375aadab18f97
Alexandra Kruchinina <krulex98@gmail.com>
2019-09-25 15:02:17 +0300

    added login page

--- covenant/server/server.js
+++ covenant/server/server.js
@@ -1,4 +1,5 @@
 'use strict';
+const  users_db = require('./users');
 
 const express = require('express');
 const path = require('path');
@@ -10,28 +11,16 @@ const staticPath = path.resolve(__dirname, '..', 'public/');
 app.use("/", express.static(staticPath));
 console.log(staticPath);
 
-const users_db = {
-    "Marshality": {
-        "email": "marshalityy@gmail.com",
-        "password": "12345",
-        "age": 21,
-    },
-
-    "Floyd": {
-        "email": "philipp-is@mail.ru",
-        "password": "privet",
-        "age": 21
-    },
-
-    "pycnick": {
-        "email": "o_nikos-gr@mail.ru",
-        "password": "juggernaut",
-        "age": 21
-    }
-};
-
 app.get('/me', (req, res) => {
     res.json(users_db["Floyd"]);
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(staticPath + '/login.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(staticPath + '/signup.html'));
 });
 
 app.listen(port, () => console.log(`server listen on port ${port}`));
