ab6e4313f38b78ca9ed8b681704f8d9ef70402ea
Alexandra Kruchinina <krulex98@gmail.com>
2019-09-28 13:19:20 +0300

    added minification images in gulpfile

--- covenant/gulpfile.js
+++ covenant/gulpfile.js
@@ -1,24 +1,50 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
+const imageMin = require('gulp-imagemin');
 const del = require('del');
 
+const path = {
+    build: {
+        js: 'public/build/js',
+        css: 'public/build/css',
+        img: 'public/build/img'
+    },
+    src: {
+        js: 'src/js/*.js',
+        css: 'src/css/*.css',
+        img: 'src/img/*.png'
+    },
+    clean: 'public/build/'
+};
+
 const styles = () => {
-    return gulp.src('src/css/*.css')
+    return gulp.src(path.src.css)
         .pipe(concat('main.css'))
         .pipe(cleanCSS({
             level: 2
         }))
-        .pipe(gulp.dest('public/build/'));
+        .pipe(gulp.dest(path.build.css));
+};
+
+const images = () => {
+    return gulp.src(path.src.img)
+        .pipe(imageMin())
+        .pipe(gulp.dest(path.build.img));
 };
 
 const scripts = () => {
-    return gulp.src('src/js/*.js')
-        .pipe(gulp.dest('public/build/'));
+    return gulp.src(path.src.js)
+        .pipe(gulp.dest(path.build.js));
 };
 
-const clean = () => del(['public/build/*']);
+const clean = () => del([path.clean]);
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
-gulp.task('build', gulp.series(clean, gulp.parallel(styles, scripts)));
+gulp.task('images', images);
+gulp.task('build', gulp.series(clean, gulp.parallel(
+        styles,
+        scripts,
+        images
+)));
