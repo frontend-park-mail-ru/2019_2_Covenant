6a8dcd46213927d894ec774c9a827152c4739d9d
shadkain <kryzev@gmail.com>
2019-10-11 21:52:56 +0300

    исправил баг реализации RAFUpdater. Теперь при повторном вызове на текущем цикле предыдущий вызов выполнен не будет (до этого терялась возможность отменить первый коллбэк)

--- base/updater/implementations/RAFUpdaterImpl.ts
+++ base/updater/implementations/RAFUpdaterImpl.ts
@@ -2,13 +2,25 @@ class RAFUpdaterImpl
 implements IUpdaterImpl {
     /** Identifier returned by requestAnimationFrame. Used to be able to cancel request */
     private _rafId: number;
+    /** Callback to be called on update */
+    private _callback: () => void;
+
+    constructor() {
+        this._update = this._update.bind(this);
+    }
 
     requestUpdate(callback: () => void) {
-        this._rafId = requestAnimationFrame(callback);
+        this._callback = callback;
+        this._rafId = requestAnimationFrame(this._update);
     }
 
     cancelUpdate() {
         cancelAnimationFrame(this._rafId);
+    }
+
+    /** Called by requestAnimationFrame */
+    private _update() {
+        this._callback();
     }
 }
 
