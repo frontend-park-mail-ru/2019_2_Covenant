2b8d94e04c5c23219b6e69d0e9628ad0537f5cfd
Alexandra Kruchinina <krulex98@gmail.com>
2019-10-17 15:53:46 +0300

    link component, header links fix

--- public/controllers/BasePageController.js
+++ public/controllers/BasePageController.js
@@ -1,9 +1,6 @@
 import BaseController from './BaseController.js';
-import EventBus from '../services/EventBus.js';
-import Events from '../services/Events.js';
+import Link from '../components/Link/Link.js';
 import Urls from '../services/Urls.js';
-
-const eventBus = new EventBus();
 
 class BasePageController extends BaseController {
 	constructor(view) {
@@ -17,29 +14,9 @@ class BasePageController extends BaseController {
 	onHide() {}
 
 	createHeaderButtons() {
-		const signup = document.getElementById('signup_link');
-		if (signup)
-			signup.addEventListener('click', this.signup);
-
-		const login = document.getElementById('login_link');
-		if (login)
-			login.addEventListener('click', this.login);
-
-		const main = document.getElementById('main_link');
-		if (main)
-			main.addEventListener('click', this.main);
-	}
-
-	signup() {
-		eventBus.publish(Events.ChangeRoute, { newUrl:  Urls.SignupUrl});
-	}
-
-	login() {
-		eventBus.publish(Events.ChangeRoute, { newUrl:  Urls.LoginUrl});
-	}
-
-	main() {
-		eventBus.publish(Events.ChangeRoute, { newUrl:  Urls.MainUrl});
+		new Link('signup_link', 'click', Urls.SignupUrl);
+		new Link('login_link', 'click', Urls.LoginUrl);
+		new Link('main_link', 'click', Urls.MainUrl);
 	}
 }
 
