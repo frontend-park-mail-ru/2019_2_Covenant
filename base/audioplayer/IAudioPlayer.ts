861c3fe130cb730536b032c1686f237d1b84b06e
Margarita <margarita@MacBook-Air-Margarita.local>
2019-10-28 16:25:40 +0300

    расширен интерфейс аудиоплеера

--- base/audioplayer/IAudioPlayer.ts
+++ base/audioplayer/IAudioPlayer.ts
@@ -1,30 +1,65 @@
 interface IAudioPlayer {
     /** Currently playing audio track */
-    readonly currentPlayback: IAudioTrack;
+    readonly currentPlayback: IPlayback;
     /** Indicates whether player is on playback */
     readonly isPlaying: boolean;
+
+    /** Order of playing */
+    shuffle: boolean;
+    /** Looping rule */
+    looping: Looping;
     /** Volume of playback */
     volume: number;
 
     /** Plays current playback */
     play(): void;
+
     /** Pauses current playback */
     pause(): void;
+
+    /** Switches current track to the next in the queue */
+    switchNext(): void;
+
+    /** Switches current track to the previous in the queue */
+    switchPrev(): void;
+
+    /**
+     * Seeks current playback to given position
+     * @param position Seek position in seconds
+     */
+    seekTo(position: number): void;
+
+    /**
+     * Seeks current playback to given normalized position
+     * @param value Normalized seek position
+     */
+    seekToNormalized(value: number): void;
+
     /**
      * Makes given track current
-     * @param track Audio track to be played
+     * @param tracks Audio track to be played
      */
     setTrack(track: IAudioTrack): void;
+
     /**
-     * Inserts track into playing queue right after current playback before next tracks in playlist
-     * @param track Track to enqueue
+     * Makes given playlist current
+     * @param playlist Playlist to be played
+     * @param head Index of track in playlist to be started with. Defaults to 0
      */
-    enqueueTrackBefore(track: IAudioTrack): void;
+    setPlaylist(playlist: IAudioTrack[], head?: number): void;
+
     /**
-     * Inserts track into playing queue after all next tracks in playlist
-     * @param track Track to enqueue
+     * Enqueues tracks at the head of the playback queue
+     * @param tracks Tracks to enqueue
      */
-    enqueueTrackAfter(track: IAudioTrack): void;
-    /** Clears playing queue */
-    clearTrackQueue(): void;
+    enqueueBefore(...tracks: IAudioTrack[]): void;
+
+    /**
+     * Enqueues tracks at the tail of the playback queue
+     * @param tracks Tracks to enqueue
+     */
+    enqueueAfter(...tracks: IAudioTrack[]): void;
+
+    /** Clears playback queue */
+    clearPlaybackQueue(): void;
 }
